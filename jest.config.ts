24a331520fa0ee0f3e08de897860f0f0782d916e
Riku <riku.block@gmail.com>
2023-09-22 17:45:59 +0200

    update configs

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,6 @@ import type { Config } from "jest";
 
 const config: Config = {
   collectCoverage: true,
-  // globalSetup: "./src/tests/setup.ts",
-  // globalTeardown: "./src/tests/teardown.ts",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   roots: ["<rootDir>/src"],
   testEnvironment: "node",
