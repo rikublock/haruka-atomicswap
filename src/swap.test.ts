b26d5ff55c8598899e5fff97f2a8a5595929d8c6
Riku <riku.block@gmail.com>
2023-09-22 14:08:14 +0200

    both unlock paths working

--- src/swap.test.ts
+++ src/swap.test.ts
@@ -41,14 +41,19 @@ describe("swap classes", () => {
     }
   });
 
-  test("swap address", async () => {
-    console.log(alice);
-    console.log(bob);
-    expect(alice.address).toBe("");
-    expect(bob.address).toBe("");
+  test("swap addresses", async () => {
+    expect(alice.address).toBe("mrG31vZtaj3WW8xzGz3ZjiCW6gaAtD6rMN");
+    expect(alice.privkey).toBe(
+      "9632f11629d05bbb9a3aef95d330b3fab6630d8133bed3efe0cc8b19191c53a9"
+    );
+
+    expect(bob.address).toBe("n4he3WuSAKJdY58ReeXtC8cMse6ip6GL1S");
+    expect(bob.privkey).toBe(
+      "0532f8eee64d878e051cb2a330428f193c6650da12a03f302c8eac826388a9a1"
+    );
   });
 
-  test("swap test", async () => {
+  test("htlc unlock with secret", async () => {
     // 5 blocks from now
     const sequence = bip68.encode({ blocks: 5 });
 
@@ -96,7 +101,6 @@ describe("swap classes", () => {
         network: regtest,
         output: redeemScript,
         input: bitcoin.script.compile([
-          // bitcoin.opcodes.OP_0,
           bitcoin.script.signature.encode(
             signer.sign(signatureHash),
             bitcoin.Transaction.SIGHASH_ALL
@@ -128,6 +132,91 @@ describe("swap classes", () => {
     // TODO verify
     console.log(await client.getRawTransaction(result));
 
+    // await regtestUtils.verify({
+    //   txId: tx.getId(),
+    //   address: regtestUtils.RANDOM_ADDRESS,
+    //   vout: 0,
+    //   value: 7e4,
+    // });
+  });
+
+  test("htlc unlock with refund", async () => {
+    // 5 blocks from now
+    const sequence = bip68.encode({ blocks: 5 });
+
+    const target = swap.createKeyPair();
+
+    const secret = swap.createSecret();
+    const redeemScript = swap.getRedeemScript(
+      secret.hash,
+      alice.pubkey,
+      bob.pubkey,
+      sequence
+    );
+    const address = swap.getRedeemAddress(redeemScript);
+
+    const value = 0.15;
+    const fee = 0.0001;
+    const txid = await client.sendToAddress(address, value);
+    console.log(txid);
+
+    const txInfo = await client.getRawTransaction(txid);
+    const vout = txInfo.vout.find(
+      (x: any) => x.scriptPubKey.address == address
+    );
+    assert(vout);
+
+    const tx = new bitcoin.Transaction();
+    tx.version = 2;
+    tx.addInput(Buffer.from(txid, "hex").reverse(), vout.n, sequence);
+    tx.addOutput(
+      bitcoin.address.toOutputScript(target.address, regtest),
+      Math.floor((value - fee) * 100000000)
+    );
+
+    const signatureHash = tx.hashForSignature(
+      0, // input index
+      redeemScript,
+      bitcoin.Transaction.SIGHASH_ALL
+    );
+
+    const signer = ECPair.fromPrivateKey(Buffer.from(alice.privkey, "hex"));
+
+    const redeemScriptSig = bitcoin.payments.p2sh({
+      network: regtest,
+      redeem: {
+        network: regtest,
+        output: redeemScript,
+        input: bitcoin.script.compile([
+          bitcoin.script.signature.encode(
+            signer.sign(signatureHash),
+            bitcoin.Transaction.SIGHASH_ALL
+          ),
+          bitcoin.opcodes.OP_0, // for the OP_IF
+        ]),
+      },
+    }).input;
+
+    tx.setInputScript(0, redeemScriptSig!);
+    // TODO set sequence
+
+    // TODO why do we have to do that
+    await client.generateToAddress(5, alice.address);
+
+    console.log(bitcoin.script.toASM(redeemScript));
+    console.log(redeemScript.toString("hex"));
+    console.log(bitcoin.script.toASM(tx.ins[0].script));
+
+    console.log(tx);
+    console.log(tx.toHex());
+
+    const result = await client.sendRawTransaction(tx.toHex());
+    console.log(result);
+
+    await client.generateToAddress(1, alice.address);
+
+    // TODO verify
+    console.log(await client.getRawTransaction(result));
 
     // await regtestUtils.verify({
     //   txId: tx.getId(),
